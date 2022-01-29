3b770a6a0a498b66289680798264fb16a07eb757
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-01-29 16:25:50 -0300

    freeCodeCamp
    Joke site from

---
+++ WebApplication2/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿    public class Joke
+    {
+
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+        public bool Isfunny { get; set; }
+        public Joke()
+        {
+
+        }
+
+
+
+    }
+
